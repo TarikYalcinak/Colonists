66d0c21bb8ae177c99af72622f29ce9aa1f7b53d
Tarik Yalcinak <tarik-2002@live.nl>
2021-06-18 00:11:26 +0200

    test

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -6,12 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+import java.nio.file.Paths;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        URL url = Paths.get("./src/main/resources/fxml/sample.fxml").toUri().toURL();
+        Parent root = FXMLLoader.load(url);
+        primaryStage.setTitle("Kolonisten van Catan");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
